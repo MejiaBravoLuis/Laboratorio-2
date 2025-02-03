72d4abdd170e1369f523b18cf684acfa0ba25ae0
MejiaBravoLuis <lmebravo6@gmail.com>
2025-02-03 11:52:02 -0600

    Reestructuración de código y subida de imágenes

--- configs/server.js
+++ configs/server.js
@@ -1,49 +1,47 @@
-'use strict'
+'use strict';
 
-import express from 'express'
-import cors from 'cors'
-import helmet from 'helmet'
-import morgan from 'morgan'
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
 import { dbConnection } from './mongo.js';
 import limiter from '../src/middlewares/validar-cant-peticiones.js';
 import authRoutes from '../src/auth/auth.routes.js'
 
-const configurarMiddlewares = (app) =>{
-    app.use(express.urlencoded({extended: false}));
+const middlewares = (app) => {
+    app.use(express.urlencoded({ extended: false }));
     app.use(cors());
     app.use(express.json());
     app.use(helmet());
     app.use(morgan('dev'));
     app.use(limiter);
-
 }
 
-const configurarRutas = (app)=>{
-    const authPath = '/adoptionSystem/v1/users';
-
-    app.use(authPath, authRoutes);
+const routes = (app) => {
+    app.use("/adoptionSystem/v1/auth", authRoutes);
 }
 
-const conectarDB = async ()=>{
+const conectarDB = async () => {
     try{
         await dbConnection();
-        console.log("Conección a la base de datos exitosa");
+        console.log("Conexión a la base de datos exitosa");
     }catch(error){
-        console.log('Error conectando a la base de datos', error);
+        console.error('Error conectando a la base de datos', error);
         process.exit(1);
     }
 }
 
-export const iniciarServidor = async () =>{
+export const initServer = async () => {
     const app = express();
-    const port = process.env.PORT || 3001;
+    const port = process.env.PORT || 3000;
 
-    await conectarDB();
-
-    configurarMiddlewares(app);
-    configurarRutas(app);
-
-    app.listen(port, () =>{
-        console.log(`Server running on port ${port}`);
-    }); 
+    try {
+        middlewares(app);
+        conectarDB();
+        routes(app);
+        app.listen(port);
+        console.log(`Server running on port: ${port}`);
+    } catch (err) {
+        console.log(`Server init failed: ${err}`);
+    }
 }
